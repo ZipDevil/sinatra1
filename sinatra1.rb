ebe265057521f879bb8eba509c57f279e6e0685e
Ubuntu <ubuntu@ip-172-31-85-204.ec2.internal>
2020-02-09 19:12:09 +0000

    sinatra1 change & update

--- sinatra1.rb
+++ sinatra1.rb
@@ -1,7 +1,24 @@
 require 'sinatra'  
+enable :sessions
 
-get '/frank-says' do
-  'Put this in your pipe & smoke it!'
+get '/' do
+  if session[:name]
+    "Welcome to Sinatra #{session[:name]}"
+  else
+    "Welcome, please log in."
+  end
+end
+
+get "/submitted" do
+  "Submitted: #params[:message]}"
+end
+
+post "/submitted" do
+  if params[:message] == "welcome"
+    "Thank you!"
+  else
+    "What do you mean by #{params[:message]}?"
+  end
 end
 
 get '/getdoc' do
@@ -9,9 +26,7 @@ get '/getdoc' do
   <html>
   <head>
     <style>
-    p {
-      text-color: red;
-    }
+    p { text-color: red; }
     </style>
   </head>
   <body>
@@ -22,16 +37,50 @@ get '/getdoc' do
   </html>"
 end
 
-get '/' do
-  "Hello, World!"
+get "/login.html" do
+  "Another method."
+end
+
+post "/login" do
+  if params[:username] == "bob" && params[:password] == "pass"
+    "<h1>SUCCESS</h1>"
+  else
+    "<h1>FAILURE</h1>"
+  end
+end
+
+post "/submitted" do
+  "<h1>Form Submitted</h1>"
+end
+
+get "/set" do
+  session[:message] = params[:message]
+  "session saved"
+end 
+
+get "/get" do
+  session[:message]
+end
+
+get "hi" do
+  session[:name] = params[:name]
+  "Name saved."
+end
+
+post "greeting" do
+  "Hi, it's very nice to meet you, #{session[:name]}"
+end
+
+get '/frank-says' do
+  'Put this in your pipe & smoke it!'
 end
 
 get '/CodeTheDream' do
-  "Hello CodeTheDream"
+  "Hello, welcome to CodeTheDream!"
 end
 
 post "/TicTacToe" do
-  "Hello TicTacToe"
+  "Hello, TicTacToe."
 end
 
 a = "yes"
